7bfa3ebc1e6f1a22364018c7c7615c8d7232e090
Mygod <mygodstudio@gmail.com>
2014-10-26 11:35:50 +0800

    Material design #2 & optimizations (2.0.1.302)

--- app/src/main/java/tk/mygod/invisibleWidgetPlus/InvisibleWidget.java
+++ app/src/main/java/tk/mygod/invisibleWidgetPlus/InvisibleWidget.java
@@ -9,21 +9,16 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.widget.RemoteViews;
 
-
-/**
- * Implementation of App Widget functionality.
- * App Widget Configuration implemented in {@link InvisibleWidgetConfigureActivity InvisibleWidgetConfigureActivity}
- */
 public class InvisibleWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for (int i = 0; i < appWidgetIds.length; ++i) update(context, appWidgetManager, appWidgetIds[i]);
+        for (int appWidgetId : appWidgetIds) update(context, appWidgetManager, appWidgetId);
     }
 
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
-        for (int i = 0; i < appWidgetIds.length; ++i) {
-            String prefix = Integer.toString(appWidgetIds[i]);
+        for (int appWidgetId : appWidgetIds) {
+            String prefix = Integer.toString(appWidgetId);
             PreferenceManager.getDefaultSharedPreferences(context).edit().remove(prefix).remove(prefix + "_name")
                              .apply();
         }
@@ -33,7 +28,7 @@ public class InvisibleWidget extends AppWidgetProvider {
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.invisible_widget);
         String prefix = Integer.toString(appWidgetId), packageName = pref.getString(prefix, "");
-        if (packageName != "") views.setOnClickPendingIntent(R.id.button, PendingIntent.getActivity
+        if (!packageName.equals("")) views.setOnClickPendingIntent(R.id.button, PendingIntent.getActivity
                 (context, 0, new Intent().setClassName(packageName, pref.getString(prefix + "_name", "")), 0));
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
--- app/src/main/java/tk/mygod/invisibleWidgetPlus/InvisibleWidgetConfigureActivity.java
+++ app/src/main/java/tk/mygod/invisibleWidgetPlus/InvisibleWidgetConfigureActivity.java
@@ -11,20 +11,16 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
+import com.melnykov.fab.FloatingActionButton;
 import tk.mygod.util.CollectionUtils;
 import tk.mygod.util.Predicate;
 
 import java.util.*;
 
-
-/**
- * The configuration screen for the {@link InvisibleWidget InvisibleWidget} AppWidget.
- */
 public class InvisibleWidgetConfigureActivity extends ActionBarActivity
         implements ExpandableListView.OnChildClickListener, AdapterView.OnItemLongClickListener {
     private static final Predicate<PackageInfo> packagePredicate = new Predicate<PackageInfo>() {
@@ -57,7 +53,7 @@ public class InvisibleWidgetConfigureActivity extends ActionBarActivity
             activitiesCounts[0] = 0;
             int i = 1, j = 0;
             for (PackageInfo info : packages) {
-                ArrayList list = CollectionUtils.filterArray(info.activities, activityPredicate);
+                ArrayList<ActivityInfo> list = CollectionUtils.filterArray(info.activities, activityPredicate);
                 activities.put(info, list);
                 activitiesCounts[i] = activitiesCounts[j] + list.size();
                 j = i++;
@@ -70,111 +66,41 @@ public class InvisibleWidgetConfigureActivity extends ActionBarActivity
                 activities = new HashMap<PackageInfo, ArrayList<ActivityInfo>>();
         private int[] activitiesCounts;
 
-        /**
-         * Gets the number of groups.
-         *
-         * @return the number of groups
-         */
         @Override
         public int getGroupCount() {
             return packages.size();
         }
 
-        /**
-         * Gets the number of children in a specified group.
-         *
-         * @param groupPosition the position of the group for which the children
-         *                      count should be returned
-         * @return the children count in the specified group
-         */
         @Override
         public int getChildrenCount(int groupPosition) {
             return activities.get(packages.get(groupPosition)).size();
         }
 
-        /**
-         * Gets the data associated with the given group.
-         *
-         * @param groupPosition the position of the group
-         * @return the data child for the specified group
-         */
         @Override
         public Object getGroup(int groupPosition) {
             return packages.get(groupPosition);
         }
 
-        /**
-         * Gets the data associated with the given child within the given group.
-         *
-         * @param groupPosition the position of the group that the child resides in
-         * @param childPosition the position of the child with respect to other
-         *                      children in the group
-         * @return the data of the child
-         */
         @Override
         public Object getChild(int groupPosition, int childPosition) {
             return activities.get(packages.get(groupPosition)).get(childPosition);
         }
 
-        /**
-         * Gets the ID for the group at the given position. This group ID must be
-         * unique across groups. The combined ID (see
-         * {@link #getCombinedGroupId(long)}) must be unique across ALL items
-         * (groups and all children).
-         *
-         * @param groupPosition the position of the group for which the ID is wanted
-         * @return the ID associated with the group
-         */
         @Override
         public long getGroupId(int groupPosition) {
             return groupPosition;
         }
 
-        /**
-         * Gets the ID for the given child within the given group. This ID must be
-         * unique across all children within the group. The combined ID (see
-         * {@link #getCombinedChildId(long, long)}) must be unique across ALL items
-         * (groups and all children).
-         *
-         * @param groupPosition the position of the group that contains the child
-         * @param childPosition the position of the child within the group for which
-         *                      the ID is wanted
-         * @return the ID associated with the child
-         */
         @Override
         public long getChildId(int groupPosition, int childPosition) {
             return activitiesCounts[groupPosition] + childPosition;
         }
 
-        /**
-         * Indicates whether the child and group IDs are stable across changes to the
-         * underlying data.
-         *
-         * @return whether or not the same ID always refers to the same object
-         */
         @Override
         public boolean hasStableIds() {
             return true;
         }
 
-        /**
-         * Gets a View that displays the given group. This View is only for the
-         * group--the Views for the group's children will be fetched using
-         * {@link #getChildView(int, int, boolean, android.view.View, android.view.ViewGroup)}.
-         *
-         * @param groupPosition the position of the group for which the View is
-         *                      returned
-         * @param isExpanded    whether the group is expanded or collapsed
-         * @param convertView   the old view to reuse, if possible. You should check
-         *                      that this view is non-null and of an appropriate type before
-         *                      using. If it is not possible to convert this view to display
-         *                      the correct data, this method can create a new view. It is not
-         *                      guaranteed that the convertView will have been previously
-         *                      created by
-         *                      {@link #getGroupView(int, boolean, android.view.View, android.view.ViewGroup)}.
-         * @param parent        the parent that this view will eventually be attached to
-         * @return the View corresponding to the group at the specified position
-         */
         @Override
         public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
             if (convertView == null)
@@ -188,29 +114,15 @@ public class InvisibleWidgetConfigureActivity extends ActionBarActivity
             return convertView;
         }
 
-        /**
-         * Gets a View that displays the data for the given child within the given
-         * group.
-         *
-         * @param groupPosition the position of the group that contains the child
-         * @param childPosition the position of the child (for which the View is
-         *                      returned) within the group
-         * @param isLastChild   Whether the child is the last child within the group
-         * @param convertView   the old view to reuse, if possible. You should check
-         *                      that this view is non-null and of an appropriate type before
-         *                      using. If it is not possible to convert this view to display
-         *                      the correct data, this method can create a new view. It is not
-         *                      guaranteed that the convertView will have been previously
-         *                      created by
-         *                      {@link #getChildView(int, int, boolean, android.view.View, android.view.ViewGroup)}.
-         * @param parent        the parent that this view will eventually be attached to
-         * @return the View corresponding to the child at the specified position
-         */
         @Override
         public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView,
                                  ViewGroup parent) {
             if (convertView == null)
                 convertView = getLayoutInflater().inflate(R.layout.icon_list_item_2, parent, false);
+            convertView.setBackgroundResource(R.color.background_darker_light);
+            int padding = (int) (56 * getResources().getDisplayMetrics().density + 0.5);
+            if (Build.VERSION.SDK_INT >= 17) convertView.setPaddingRelative(padding, 0, 0, 0);
+            else convertView.setPadding(padding, 0, 0, 0);
             ActivityInfo info = activities.get(packages.get(groupPosition)).get(childPosition);
             PackageManager manager = getPackageManager();
             ((ImageView) convertView.findViewById(android.R.id.icon)).setImageDrawable(info.loadIcon(manager));
@@ -220,13 +132,6 @@ public class InvisibleWidgetConfigureActivity extends ActionBarActivity
             return convertView;
         }
 
-        /**
-         * Whether the child at the specified position is selectable.
-         *
-         * @param groupPosition the position of the group that contains the child
-         * @param childPosition the position of the child within the group
-         * @return whether the child is selectable.
-         */
         @Override
         public boolean isChildSelectable(int groupPosition, int childPosition) {
             return true;
@@ -264,43 +169,27 @@ public class InvisibleWidgetConfigureActivity extends ActionBarActivity
         list.setEmptyView(findViewById(android.R.id.empty));
         list.setOnChildClickListener(this);
         list.setOnItemLongClickListener(this);
+        ((FloatingActionButton) findViewById(R.id.fab)).attachToListView(list);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.configure_menu, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.do_nothing:
-                save(PreferenceManager.getDefaultSharedPreferences(this).edit()
-                        .putString(Integer.toString(widgetId), ""));
-                setResult(RESULT_OK, new Intent().putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId));
-            case android.R.id.home:
-                finish();
-                return true;
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * Callback method to be invoked when a child in this expandable list has
-     * been clicked.
-     *
-     * @param parent The ExpandableListView where the click happened
-     * @param v The view within the expandable list/ListView that was clicked
-     * @param groupPosition The group position that contains the child that
-     *        was clicked
-     * @param childPosition The child position within the group
-     * @param id The row id of the child that was clicked
-     * @return True if the click was handled
-     */
+    public void doNothing(View view) {
+        save(PreferenceManager.getDefaultSharedPreferences(this).edit()
+                .putString(Integer.toString(widgetId), ""));
+        setResult(RESULT_OK, new Intent().putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId));
+        finish();
+    }
+
     @Override
     public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
         ActivityInfo info = (ActivityInfo) adapter.getChild(groupPosition, childPosition);
@@ -313,24 +202,12 @@ public class InvisibleWidgetConfigureActivity extends ActionBarActivity
         return true;
     }
 
-    /**
-     * Callback method to be invoked when an item in this view has been
-     * clicked and held.
-     * <p/>
-     * Implementers can call getItemAtPosition(position) if they need to access
-     * the data associated with the selected item.
-     *
-     * @param parent   The AbsListView where the click happened
-     * @param view     The view within the AbsListView that was clicked
-     * @param position The position of the view in the list
-     * @param id       The row id of the item that was clicked
-     * @return true if the callback consumed the long click, false otherwise
-     */
     @Override
     public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
         if (ExpandableListView.getPackedPositionType(id) == ExpandableListView.PACKED_POSITION_TYPE_CHILD) {
-            ActivityInfo info = (ActivityInfo) adapter.getChild(ExpandableListView.getPackedPositionGroup(id),
-                                                                ExpandableListView.getPackedPositionChild(id));
+            int groupPosition = ExpandableListView.getPackedPositionGroup(id);
+            ActivityInfo info = (ActivityInfo) adapter.getChild(groupPosition,
+                    ExpandableListView.getPackedPositionChild(id) - adapter.activitiesCounts[groupPosition]);
             startActivity(new Intent().setClassName(info.packageName, info.name));
             return true;
         }
@@ -342,6 +219,3 @@ public class InvisibleWidgetConfigureActivity extends ActionBarActivity
         else editor.commit();
     }
 }
-
-
-
